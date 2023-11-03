3c4850f11d04bf3500296bdfe9783ec621ffb712
Piotr Sowa <piotrsowa87@gmail.com>
2023-11-03 08:56:08 +0100

    Display selected articles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const titleClickHandler = function(event) {
-    console.log('link clicked: ', event);
+    event.preventDefault();
+
+    const clickedElement = this;
+    console.log('link clicked: ', event , clickedElement);
 
     // remove class active from all links
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -9,6 +12,7 @@ const titleClickHandler = function(event) {
         activeLink.classList.remove('active');
     }
     // add class active to the clicked link
+    clickedElement.classList.add('active');
 
     // remove class active from all articles
     const activeArticles = document.querySelectorAll('.posts article.active')
@@ -16,11 +20,12 @@ const titleClickHandler = function(event) {
         activeArticle.classList.remove('active');
     }
     // get href attribute from clicked link
-
+    const articleId = clickedElement.getAttribute('href');
+    console.log('Article id: ', articleId);
     // find the correct article using selector by id
-
+    const selectedArticle = document.querySelector(articleId);
     // add class active to the target article
-
+    selectedArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
