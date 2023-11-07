df3ae50a051fe6421cb7008ae7319bf719823bae
Piotr Sowa <piotrsowa87@gmail.com>
2023-11-07 15:50:30 +0100

    Add style and validators

--- js/script.js
+++ js/script.js
@@ -28,9 +28,42 @@ const titleClickHandler = function(event) {
     selectedArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+{
+    const optArticleSelector = '.post',
+        optTitleSelector = '.post-title',
+        optTitleListSelector = '.titles';
 
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+    function generateTitleLinks(){
+
+        /* remove contents of titleList */
+        const titleList = document.querySelector(optTitleListSelector);
+        titleList.innerHTML = '';
+
+        /* for each article */
+        let html = "";
+        const articles = document.querySelectorAll(optArticleSelector);
+        for (let article of articles) {
+        /* get the article id */
+            const articleId = article.getAttribute('id');
+        /* find the title element */
+            const articleElement = article.querySelector(optTitleSelector);
+        /* get the title from the title element */
+            const articleTitle = articleElement.innerHTML;
+        /* create HTML of the link */
+            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+            console.log(linkHTML);
+        /* insert link into titleList */
+            //titleList.insertAdjacentHTML("beforeend", linkHTML);
+            html = html + linkHTML;
+        }
+        titleList.innerHTML = html;
+
+        const links = document.querySelectorAll('.titles a');
+
+        for (let link of links) {
+            link.addEventListener('click', titleClickHandler);
+        }
+    }
+
+    generateTitleLinks();
 }
-
